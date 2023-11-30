683a3b34352df92ecd05816512b05159df9d5a60
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-11-30 23:58:56 +0530

    transaction in jpa

--- JPA/JPADemo/jpademo/src/main/java/com/nabajyoti/jpa/App.java
+++ JPA/JPADemo/jpademo/src/main/java/com/nabajyoti/jpa/App.java
@@ -1,5 +1,9 @@
 package com.nabajyoti.jpa;
 
+import jakarta.persistence.Query;
+
+import java.util.List;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -11,5 +15,37 @@ public class App {
         EntityManager em = emf.createEntityManager();
         User user = em.find(User.class, "naba93980@gmail.com");
         System.out.println(user);
+
+        User user2 = new User();
+        user2.setFirstName("biplab");
+        user2.setLastName("sarkar");
+        user2.setEmail("biplab@gmail.com");
+
+        try {
+            // Persist entities, perform other operations
+            em.getTransaction().begin();
+            em.persist(user2);
+            
+            String jpql = "SELECT e FROM User e";
+            Query query = em.createQuery(jpql);
+            
+            // Execute the query and return the result list
+            List result = query.getResultList();
+            for (Object r : result) {
+                System.out.println(r);
+            }
+            
+            User akash = em.find(User.class, "biplab@gmail.com");
+            em.remove(akash);
+            em.getTransaction().commit(); // Commit the transaction
+            
+        } catch (Exception e) {
+            if (em != null && em.getTransaction().isActive()) {
+                em.getTransaction().rollback(); // Rollback if an exception occurs
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
     }
 }
